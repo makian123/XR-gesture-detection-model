eef65d2ade307ceb380d9e368e043fbbd2f2f662
Makian <31448957+makian123@users.noreply.github.com>
2026-03-22 19:54:09 +0100

    Update watch_nvidiasmi.py

--- watch_nvidiasmi.py
+++ watch_nvidiasmi.py
@@ -1,7 +1,7 @@
 import subprocess, time, os
 
 while True:
-    os.system("cls")  # clears the VS Code terminal
+    os.system("cls" if os.name=="nt" else "clear")  # clears the VS Code terminal
     output = subprocess.run(["nvidia-smi"], capture_output=True, text=True)
     print(output.stdout)
     time.sleep(1)
